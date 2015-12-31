5d3b51aaee95093aee6e41043809021a86edb3f4
alpeshmail@gmail.com <alpeshmail@gmail.com>
2015-12-30 22:29:37 -0500

    added new project

---
+++ MvcApplication2/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+
+namespace MvcApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+           
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var json = config.Formatters.JsonFormatter;
+
+            json.SerializerSettings.Formatting = Formatting.Indented;
+            json.SerializerSettings.TypeNameHandling = TypeNameHandling.Objects;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
---
+++ MvcApplication2/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using AttributeRouting;
+using AttributeRouting.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication2.Controllers
+{
+    [RoutePrefix("api/test5")]
+    public class TestController : ApiController
+    {
+        // GET api/test
+        [GET("all")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/test/5
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
